901ac8b0570d298c70752be1b7d9fe1894aedb26
sinaastani <sinaastani@gmail.com>
2018-05-04 00:03:47 -0700

    Initial Commit

---
+++ Kilobots_proj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Kilobots_proj
+//
+//  Created by Sina on 5/4/18.
+//  Copyright © 2018 Sina. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
